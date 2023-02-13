a4d913f4b8ae93779b38aff251a6a92d3c45d3a7
Agnieszka Nowacka <agnesnowacka1@gmail.com>
2023-02-13 22:54:11 +0100

    added themeProvider and created file theme.js

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+    color: {
+        primaryColor: "#047B6C",
+        silver: "#CCCCCC",
+        white: "#FFFFFF",
+        gallery: "#ECEAEA",
+        forestGreen: "#228C22",
+        mexicanRed: "#b42222",
+
+    },
+
+    breakpoint: {
+        mobileMax: 767,
+    },
+
+};
+
