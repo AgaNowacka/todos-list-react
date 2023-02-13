3f352022b53105a7350024f32e824946f6e3b34d
Agnieszka Nowacka <agnesnowacka1@gmail.com>
2023-02-13 17:48:50 +0100

    deleted useState from Tasks.js, changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
 import Tasks from "./features/tasks/Tasks";
 
 export default () => <Tasks />;
+
+
--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -4,34 +4,20 @@ import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import Container from "../../common/Container";
-import { useState } from 'react';
-import { useTasks } from "../../useTasks";
+
 
 function Tasks() {
-
-  const [hideDoneTasks, setHideDoneTasks] = useState(false);
-
-  const toggleHideDoneTasks = () => {
-    setHideDoneTasks(hideDoneTasks => !hideDoneTasks);
-  };
-
-  const {
-    tasks,
-    removeTask,
-    toggleTaskDone,
-    setAllDone,
-    addNewTask,
-  } = useTasks();
-
-
   return (
     <Container>
       <Header title="Lista zadań" />
-      <Section title="Dodaj nowe zadanie" body={<Form addNewTask={addNewTask} />} />
-      <Section title="Lista zadań" body={
-        <TasksList tasks={tasks} hideDoneTasks={hideDoneTasks} removeTask={removeTask} toggleTaskDone={toggleTaskDone} />}
-        extraHeaderContent={
-          <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} toggleHideDoneTasks={toggleHideDoneTasks} setAllDone={setAllDone} />}
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+      />
+      <Section
+        title="Lista zadań"
+        body={<TasksList />}
+        extraHeaderContent={<Buttons />}
       />
     </Container>
   );
